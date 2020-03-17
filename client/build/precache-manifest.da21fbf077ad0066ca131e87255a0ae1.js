03bc8134a42c0624aa0fd2b4a204673f49f57a5c
Tuan Le <letuanlt09@gmail.com>
2020-03-16 22:51:10 -0400

    make it better

---
+++ client/build/precache-manifest.da21fbf077ad0066ca131e87255a0ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31865a69e7ca5b49f4b3c7860b0646a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f63e285e66a77a9572c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "68a6471de1b83e6f41e3",
+    "url": "/static/js/2.851f3c5f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.851f3c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f63e285e66a77a9572c",
+    "url": "/static/js/main.42f0296f.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
